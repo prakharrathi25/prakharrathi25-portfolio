11d0f9fc27693b747403f7cb05430ae24e34139e
Prakhar Rathi <prakharrathi25@gmail.com>
2020-06-01 18:09:14 +0530

    New Project section +
    Content Added as well as a new project section with better styling.

--- js/index.js
+++ js/index.js
@@ -10,3 +10,20 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+
+// JS Script for the work section 
+const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+
+portfolioItems.forEach(portfolioItem => {
+portfolioItem.addEventListener('mouseover', () => {
+  console.log(portfolioItem.childNodes[1].classList)
+  portfolioItem.childNodes[1].classList.add('image-blur');
+});
+
+portfolioItem.addEventListener('mouseout', () => {
+  console.log(portfolioItem.childNodes[1].classList)
+  portfolioItem.childNodes[1].classList.remove('image-blur');
+});
+});
+
